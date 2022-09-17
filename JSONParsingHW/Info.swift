2050272c7b9a7151da9f12b8298f0ac5e204231e
Anton Zarichny <antonzarichny@gmail.com>
2022-09-17 15:30:54 +0300

    HW

---
+++ JSONParsingHW/Info.swift
@@ -0,0 +1,14 @@
+//
+//  Info.swift
+//  JSONParsingHW
+//
+//  Created by Антон Заричный on 17.09.2022.
+//
+
+import Foundation
+
+struct Info: Decodable {
+    let characters: String?
+    let locations: String?
+    let episodes: String?
+}
--- JSONParsingHW/ViewController.swift
+++ JSONParsingHW/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
+let jsonInfo = "https://rickandmortyapi.com/api"
+
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchInfo()
     }
+}
 
-
+private func fetchInfo() {
+    guard let url = URL(string: jsonInfo) else { return }
+    
+    URLSession.shared.dataTask(with: url) { data, _, error in
+        guard let data = data else {
+            print(error?.localizedDescription ?? "No error description")
+            return
+        }
+        
+        let jsonDecoder = JSONDecoder()
+        
+        do {
+            let jsonInfo = try jsonDecoder.decode(Info.self, from: data)
+            print(jsonInfo)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }.resume()
 }
 
